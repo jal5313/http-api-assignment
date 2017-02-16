5c253c4f5e0e9587630f4bba9b9302dc6b70bce9
Josh Ladisic <jal5313@gmail.com>
2017-02-16 18:37:13 -0500

    basic setup, client page running

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const cssHandler = require('./cssResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      cssHandler.getStyle(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
